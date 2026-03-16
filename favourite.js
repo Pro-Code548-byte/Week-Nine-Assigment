2127a83171bf3e671d48e3dfdffab98f01af04db
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-16 11:50:47 +0100

    Adding the first command

--- favourite.js
+++ favourite.js
@@ -1,2 +1,7 @@
 const favColor = "Black";
-const favFood = "";
+const favFood = "Extreme Biscuits";
+const favSport = "Swimming";
+const favMovie = "XO Kitty";
+console.log(
+  `My favourite color is ${favColor}, I love eating ${favFood}, I enjoy ${favSport}, and my favourite movie is ${favMovie}.`,
+);
