fed5f0ac3856e0549dd42d83ad13292f8a8571b9
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-16 14:39:01 +0100

    Completion of eleventh command

--- stringbuild.js
+++ stringbuild.js
@@ -3,7 +3,17 @@ let count = 6;
 let result = "";
 
 for (let i = 1; i <= count; i++) {
-    result += char;
+  result += char;
+}
+
+console.log(result);
+
+char = "_";
+count = 10;
+result = "";
+
+for (let i = 1; i <= count; i++) {
+  result += char;
 }
 
 console.log(result);
