78c2ffe2a7d19a77a96452cdd56b453c01a946c7
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-16 14:02:56 +0100

    Adding the tenth command

---
+++ numtoword.js
@@ -0,0 +1,149 @@
+digit = 4;
+if (digit === 0) {
+  console.log("Zero");
+} else if (digit === 1) {
+  console.log("One");
+} else if (digit === 2) {
+  console.log("Two");
+} else if (digit === 3) {
+  console.log("Three");
+} else if (digit === 4) {
+  console.log("Four");
+} else if (digit === 5) {
+  console.log("Five");
+} else if (digit === 6) {
+  console.log("Six");
+} else if (digit === 7) {
+  console.log("Seven");
+} else if (digit === 8) {
+  console.log("Eight");
+} else if (digit === 9) {
+  console.log("Nine");
+} else {
+  console.log("Unknown");
+}
+
+digit = 0;
+if (digit === 0) {
+  console.log("Zero");
+} else if (digit === 1) {
+  console.log("One");
+} else if (digit === 2) {
+  console.log("Two");
+} else if (digit === 3) {
+  console.log("Three");
+} else if (digit === 4) {
+  console.log("Four");
+} else if (digit === 5) {
+  console.log("Five");
+} else if (digit === 6) {
+  console.log("Six");
+} else if (digit === 7) {
+  console.log("Seven");
+} else if (digit === 8) {
+  console.log("Eight");
+} else if (digit === 9) {
+  console.log("Nine");
+} else {
+  console.log("Unknown");
+}
+
+digit = 3;
+if (digit === 0) {
+  console.log("Zero");
+} else if (digit === 1) {
+  console.log("One");
+} else if (digit === 2) {
+  console.log("Two");
+} else if (digit === 3) {
+  console.log("Three");
+} else if (digit === 4) {
+  console.log("Four");
+} else if (digit === 5) {
+  console.log("Five");
+} else if (digit === 6) {
+  console.log("Six");
+} else if (digit === 7) {
+  console.log("Seven");
+} else if (digit === 8) {
+  console.log("Eight");
+} else if (digit === 9) {
+  console.log("Nine");
+} else {
+  console.log("Unknown");
+}
+
+digit = 7;
+if (digit === 0) {
+  console.log("Zero");
+} else if (digit === 1) {
+  console.log("One");
+} else if (digit === 2) {
+  console.log("Two");
+} else if (digit === 3) {
+  console.log("Three");
+} else if (digit === 4) {
+  console.log("Four");
+} else if (digit === 5) {
+  console.log("Five");
+} else if (digit === 6) {
+  console.log("Six");
+} else if (digit === 7) {
+  console.log("Seven");
+} else if (digit === 8) {
+  console.log("Eight");
+} else if (digit === 9) {
+  console.log("Nine");
+} else {
+  console.log("Unknown");
+}
+
+digit = 9;
+if (digit === 0) {
+  console.log("Zero");
+} else if (digit === 1) {
+  console.log("One");
+} else if (digit === 2) {
+  console.log("Two");
+} else if (digit === 3) {
+  console.log("Three");
+} else if (digit === 4) {
+  console.log("Four");
+} else if (digit === 5) {
+  console.log("Five");
+} else if (digit === 6) {
+  console.log("Six");
+} else if (digit === 7) {
+  console.log("Seven");
+} else if (digit === 8) {
+  console.log("Eight");
+} else if (digit === 9) {
+  console.log("Nine");
+} else {
+  console.log("Unknown");
+}
+
+digit = 11;
+if (digit === 0) {
+  console.log("Zero");
+} else if (digit === 1) {
+  console.log("One");
+} else if (digit === 2) {
+  console.log("Two");
+} else if (digit === 3) {
+  console.log("Three");
+} else if (digit === 4) {
+  console.log("Four");
+} else if (digit === 5) {
+  console.log("Five");
+} else if (digit === 6) {
+  console.log("Six");
+} else if (digit === 7) {
+  console.log("Seven");
+} else if (digit === 8) {
+  console.log("Eight");
+} else if (digit === 9) {
+  console.log("Nine");
+} else {
+  console.log("Unknown");
+}
