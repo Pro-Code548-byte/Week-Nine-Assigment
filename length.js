8fcba584216d2dba0e20282529ef406a72070f3d
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-16 12:42:54 +0100

    Adding the sixth command

---
+++ length.js
@@ -0,0 +1,44 @@
+let word = "programming";
+if (word.length < 5) {
+  console.log("Short");
+} else if (word.length >= 5 && word.length <= 8) {
+  console.log("Medium");
+} else {
+  console.log("Long");
+}
+
+word = "hi";
+if (word.length < 5) {
+  console.log("Short");
+} else if (word.length >= 5 && word.length <= 8) {
+  console.log("Medium");
+} else {
+  console.log("Long");
+}
+
+word = "hello";
+if (word.length < 5) {
+  console.log("Short");
+} else if (word.length >= 5 && word.length <= 8) {
+  console.log("Medium");
+} else {
+  console.log("Long");
+}
+
+word = "language";
+if (word.length < 5) {
+  console.log("Short");
+} else if (word.length >= 5 && word.length <= 8) {
+  console.log("Medium");
+} else {
+  console.log("Long");
+}
+
+word = "JavaScript";
+if (word.length < 5) {
+  console.log("Short");
+} else if (word.length >= 5 && word.length <= 8) {
+  console.log("Medium");
+} else {
+  console.log("Long");
+}
